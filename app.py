63f0248fe04fa373339f5e984239088a94792d1a
Mason <mla290@sfu.ca>
2026-01-08 03:16:38 -0800

    git status

--- app.py
+++ app.py
@@ -4,20 +4,21 @@ layers = tf.keras.layers
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+from typing import Tuple
 
-
-dataset_path = "tom_and_jerry_training_dataset"
-dataset_testing_path = "tom_and_jerry_testing_dataset"
-
-for root, dirs, files in os.walk(dataset_testing_path):
-    for file in files:
-        if file == ".DS_Store":
-            os.remove(os.path.join(root, file))
-            print(f"Deleted: {os.path.join(root, file)}")
-
+DATASET_PATH = "tom_and_jerry_training_dataset"
+DATASET_TESTING_PATH = "tom_and_jerry_testing_dataset"
 IMAGE_SIZE = (64, 64)  # Define the target image size
+SINGLE_IMAGE_TESTS_PATH = "single_image_tests"
 
-def debug_image(dataset_path):
+def clean_dataset(dataset_path) -> None:
+    for root, dirs, files in os.walk(dataset_path):
+        for file in files:
+            if file == ".DS_Store":
+                os.remove(os.path.join(root, file))
+                print(f"Deleted: {os.path.join(root, file)}")
+
+def debug_image(dataset_path) -> None:
     image = tf.io.read_file(dataset_path)
     image = tf.image.decode_image(image, channels=3)
     image = tf.image.resize(image, IMAGE_SIZE)
@@ -25,93 +26,88 @@ def debug_image(dataset_path):
     plt.imshow(image.numpy().astype("uint8"))  # Convert to uint8 before displaying
     plt.show()
 
-#debug_image("tom_and_jerry_training_dataset/tom/frame368.jpg")
+def setup_datasets() -> Tuple[tf.data.Dataset, tf.data.Dataset]:
+    # Define train-validation split
+    train_size = 0.8  # 80% training, 20% validation
+    val_size = 1 - train_size
 
+    # Split dataset
+    #train_dataset = train_dataset.take(train_batches)
+
+    train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+        DATASET_PATH,
+        image_size=IMAGE_SIZE,
+        batch_size=64,
+        validation_split=0.2,  # Use 20% for validation
+        subset="training",
+        seed=123,  # Ensures consistent split
+        interpolation="nearest"  
+    ) 
             
-# train_dataset = train_dataset.shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+    normalization_layer = tf.keras.layers.Rescaling(1./255)
+    train_dataset = train_dataset.map(lambda x, y: (normalization_layer(x), y))
 
-
-# Define train-validation split
-train_size = 0.8  # 80% training, 20% validation
-val_size = 1 - train_size
-
-# Split dataset
-#train_dataset = train_dataset.take(train_batches)
-
-train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
-    dataset_path,
-    image_size=IMAGE_SIZE,
-    batch_size=64,
-    validation_split=0.2,  # Use 20% for validation
-    subset="training",
-    seed=123,  # Ensures consistent split
-    interpolation="nearest"  
-) 
         
-print("Class Names:", train_dataset.class_names)
 
-normalization_layer = tf.keras.layers.Rescaling(1./255)
-train_dataset = train_dataset.map(lambda x, y: (normalization_layer(x), y))
+    val_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+        DATASET_PATH,
+        image_size=IMAGE_SIZE,
+        batch_size=64,
+        validation_split=0.2,  # Use same split
+        subset="validation",
+        seed=123,
+        interpolation="nearest" 
+    )
+    #val_dataset = train_dataset.skip(train_batches)
+    normalization_layer = tf.keras.layers.Rescaling(1./255)
+    val_dataset = val_dataset.map(lambda x, y: (normalization_layer(x), y))
 
+
+
+    # Get total number of batches
+    total_batches = len(train_dataset)
+
+    # Calculate number of training batches
+    train_batches = int(total_batches * train_size)
     
-
-val_dataset = tf.keras.preprocessing.image_dataset_from_directory(
-    dataset_path,
-    image_size=IMAGE_SIZE,
-    batch_size=64,
-    validation_split=0.2,  # Use same split
-    subset="validation",
-    seed=123,
-    interpolation="nearest" 
-)
-#val_dataset = train_dataset.skip(train_batches)
-normalization_layer = tf.keras.layers.Rescaling(1./255)
-val_dataset = val_dataset.map(lambda x, y: (normalization_layer(x), y))
-
-
-
-# Get total number of batches
-total_batches = len(train_dataset)
-
-# Calculate number of training batches
-train_batches = int(total_batches * train_size)
-
-
-
-# Define class names
-#class_names = ['jerry','tom']  # Update class names to match your dataset
+    return train_dataset, val_dataset
 
 # Build CNN model
 
-model = keras.Sequential([
-        keras.Input(shape=(64, 64, 3)),  # Define input explicitly
-        layers.Conv2D(32, (3, 3), activation='relu'),
-        layers.Conv2D(32, (3, 3), activation='relu'),
-        layers.MaxPooling2D(2, 2),  # Reduce size
-        layers.Conv2D(32, (3, 3), activation='relu'),
+def create_model() -> tf.keras.Model:
 
-        layers.GlobalAveragePooling2D(),
-        #layers.Flatten(),
-        layers.Dense(64, activation='relu'),
-        layers.Dropout(0.5),  # Helps prevent overfitting
-        layers.Dense(2, activation='softmax')
-])
+    model = keras.Sequential([
+            keras.Input(shape=(64, 64, 3)),  # Define input explicitly
+            layers.Conv2D(32, (3, 3), activation='relu'),
+            layers.Conv2D(32, (3, 3), activation='relu'),
+            layers.MaxPooling2D(2, 2),  # Reduce size
+            layers.Conv2D(32, (3, 3), activation='relu'),
 
-# Compile the model
-model.compile(optimizer='adam',
-            loss='sparse_categorical_crossentropy',
-            metrics=['accuracy'])
+            layers.GlobalAveragePooling2D(),
+            #layers.Flatten(),
+            layers.Dense(64, activation='relu'),
+            layers.Dropout(0.5),  # Helps prevent overfitting
+            layers.Dense(2, activation='softmax')
+    ])
 
-model.summary()
+    # Compile the model
+    model.compile(optimizer='adam',
+                loss='sparse_categorical_crossentropy',
+                metrics=['accuracy'])
 
-# Train the model
-history = model.fit(train_dataset, validation_data=val_dataset, epochs=20)
+    model.summary()
+    
+    return model
 
-# Evaluate the model
-val_loss, val_acc = model.evaluate(val_dataset)
-print(f"Validation Accuracy: {val_acc:.2f}")
+def train_evaluate_save_model(model, train_dataset, val_dataset) -> None:
+    # Train the model
+    history = model.fit(train_dataset, validation_data=val_dataset, epochs=20)
 
-model.save("tom_and_jerry_classifier.h5")
+    # Evaluate the model
+    val_loss, val_acc = model.evaluate(val_dataset)
+    print(f"Validation Accuracy: {val_acc:.2f}")
+
+    model.save("tom_and_jerry_classifier.h5")
 
 
 def predict_images_in_directory(directory_path, model):
@@ -167,11 +163,10 @@ def predict_images_in_directory(directory_path, model):
     
     return predictions  # Return all results as a dictionary
 
-model = keras.models.load_model("tom_and_jerry_classifier.h5")
+def load_model() -> tf.keras.Model:
+    return keras.models.load_model("tom_and_jerry_classifier.h5")
 
-results = predict_images_in_directory(dataset_testing_path, model)
-
-def predict_single_image(image_path, model):
+def predict_single_image(image_path, model) -> None:
     class_names = ["jerry", "tom"]  # Same class label order used during training
 
     try:
@@ -189,9 +184,12 @@ def predict_single_image(image_path, model):
     except Exception as e:
         print(f"Error processing {image_path}: {e}")
         return None
-    
-for_fun_result = predict_single_image("test.jpg", model)
-print("For fun result: ", for_fun_result)
+
+def try_single_images() -> None:
+    loaded_model = load_model()
+    for filename in os.listdir(SINGLE_IMAGE_TESTS_PATH):
+        file_path = os.path.join(SINGLE_IMAGE_TESTS_PATH, filename)
+        predict_single_image(file_path, loaded_model)
 
 
 
